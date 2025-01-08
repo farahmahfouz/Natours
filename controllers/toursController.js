468867cfcd1eea34bec91405a034af2249ba0deb
farahmahfouz <farahmahfouz11@gmail.com>
2025-01-09 01:36:35 +0200

    Feat/ Add Multipile Images

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -26,10 +26,32 @@ exports.uploadTourImages = upload.fields([
   { name: 'images', maxCount: 3 },
 ]);
 
-exports.resizeToursImages = (req, res, next) => {
-  console.log(req.files);
-  next()
-}
+exports.resizeToursImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
+
+  const imageCoverFilename = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${imageCoverFilename}`);
+  req.body.imageCover = imageCoverFilename;
+
+  req.body.images = [];
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+      req.body.images.push(filename);
+    })
+  );
+
+  next();
+});
 
 // upload.array('images', 5)
 exports.aliasTopTours = (req, res, next) => {
